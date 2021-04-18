116dc99389572686bb7e25eafc81d0dc5aa70b1d
insomnimus <insomnimus19@gmail.com>
2021-04-18 21:53:01 +0300

    'add --all flag'

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,81 @@
 use std::env;
 use std::path::Path;
+use std::process;
 
 fn show_help() {
-    let cmd = match env::current_exe() {
-        Ok(p) => match p.file_name() {
-            Some(s) => s.to_str().unwrap().to_owned(),
-            None => String::from("which"),
-        },
-        Err(_) => String::from("which"),
-    };
+	let cmd = match env::current_exe() {
+		Ok(p) => match p.file_name() {
+			Some(s) => s.to_str().unwrap().to_owned(),
+			None => String::from("which"),
+		},
+		Err(_) => String::from("which"),
+	};
 
-    eprintln!(
-        "{}, finds executables under $PATH
+	eprintln!(
+		"{}, finds executables under $PATH
 usage:
-	{} <filename>",
-        cmd, cmd
-    );
-    std::process::exit(0);
+	{} [options] <filename>
+options:
+	-a, --all: print every match instead of just the first
+	-h, --help: show this message and exit",
+		cmd, cmd
+	);
+	std::process::exit(0);
 }
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() <= 1 {
-        show_help();
-    }
-    let arg = &args[1];
-
-    if arg == "-h" || arg == "--help" {
-        show_help();
-    }
-    let target: String = if str::ends_with(&arg, ".exe") {
-        arg.to_owned()
-    } else {
-        format!("{}.exe", arg).to_owned()
-    };
-
-    match env::var_os("PATH") {
-        Some(paths) => {
-            for path in env::split_paths(&paths) {
-                //let p= format!("{}/{}", &path.display(), &target);
-                //println!("{}", p);
-                if Path::new(&format!("{}/{}", path.display(), &target)).exists() {
-                    println!("{}\\{}", path.display(), &target);
-                    return;
-                }
-            }
-        }
-        None => panic!("can't get the value of $PATH"),
-    }
-    eprintln!("{}: not found", arg);
-    std::process::exit(1);
+	let args: Vec<_> = env::args().collect();
+	if args.len() <= 1 {
+		show_help();
+	}
+	let mut target= "";
+	let mut flag_all=false;
+	
+	for a in &args[1..]{
+		match &a[..]{
+			"-h"|"--help"=> show_help(),
+			"-a"|"--all"=> flag_all=true,
+			_=> {
+				if target!= ""{
+					eprintln!("too many arguments, run witth --help for the usage");
+					process::exit(2);
+				}
+				target= &a[..];
+			}
+		}
+	}
+	if target=="" && flag_all{
+		eprintln!("missing argument: command to search for");
+		process::exit(2);
+	}
+	if target== ""{
+		show_help();
+	}
+	
+	let target= if target.ends_with(".exe") {
+		target.to_owned()
+	}else{
+		format!("{}.exe", target)
+	};
+	
+	let mut found=false;
+	match env::var_os("PATH") {
+		Some(paths) => {
+			for path in env::split_paths(&paths) {
+				if Path::new(&format!("{}/{}", path.display(), &target)).exists() {
+					println!("{}\\{}", path.display(), &target);
+					if !flag_all{
+						return;
+					}
+					found=true;
+				}
+			}
+		}
+		None => panic!("can't get the value of $PATH"),
+	}
+	if found{
+		return;
+	}
+	eprintln!("{}: not found", target);
+	process::exit(1);
 }
